0ed20f058c822f9ad7a66a547e860bc1a396c99d
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-05-22 12:15:50 +0200

    Add Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='footer__container'>
+        <div className='footer__title'>
+          FastFingers
+        </div>
+        <div className='copyright'>
+          &copy;2023 - Czechitas, Digitální akademie: Web
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="container">
       <Header />
+      <Footer />
     </div>
   );
 };
